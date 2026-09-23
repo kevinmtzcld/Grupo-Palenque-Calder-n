2365782c73b5ba414b4046cd58421498568f95f1
kevinmtzcld <109487838+kevinmtzcld@users.noreply.github.com>
2026-09-23 11:24:30 -0300

    Create tokens.h

---
+++ tokens.h
@@ -0,0 +1,47 @@
+
+#ifndef TOKENS_H
+#define TOKENS_H
+
+// Palabras Reservadas
+#define PR_DEF           257
+#define PR_PRINCIPAL     258
+#define PR_RACIONAL      259
+#define PR_SI            260
+#define PR_SINO          261
+#define PR_MIENTRAS      262
+#define PR_MOSTRAR       263
+#define PR_RETORNAR      264
+
+// Identificadores y Literales
+#define ID               265
+#define LIT_RACIONAL     266
+#define LIT_CADENA       267
+
+// Operadores
+#define OP_ASIG          268
+#define OP_SUMA          269
+#define OP_RESTA         270
+#define OP_MULT          271
+#define OP_DIV           272
+
+// Comparadores
+#define COMP_MAYOR       273
+#define COMP_MENOR       274
+#define COMP_MAYOR_IGUAL 275
+#define COMP_MENOR_IGUAL 276
+#define COMP_IGUAL       277
+#define COMP_DISTINTO    278
+
+// Operadores Lógicos
+#define OP_AND           279
+#define OP_OR            280
+
+// Delimitadores y Símbolos
+#define PAR_IZQ          281
+#define PAR_DER          282
+#define LLAVE_IZQ        283
+#define LLAVE_DER        284
+#define PUNTO_COMA       285
+#define COMA             286
+
+#endif // TOKENS_H
